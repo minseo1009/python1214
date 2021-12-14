ffe1e078a265e64761c55984939d4520f3580c71
minseo1009 <minseo@kribb.re.kr>
2021-12-14 10:21:38 +0900

    first

---
+++ demo.py
@@ -0,0 +1,38 @@
+# demo.py
+print("hello python")
+
+# 자료 형식
+# 리스트 [] : ["ham", "spam"] - 순서 있음 (입력,수정,삭제,검색)
+# 사전(dict) {key:value} : {"ham":4, "spam":5} - 순서 없음 (해시함수, 맵핑 구조, key:value) - JSON, NFT
+# 튜플 () : ("ham", "spam") - 순서 있음 (변경 안됨)
+# 세트(set) {} : {1,2,3,4} - 순서 없음 (집합 연산, Unique)
+
+
+# 자료형
+# \n 개행
+# \t 탭
+# \\ \문자
+# \0 널 (Python에서는 None으로 사용) - 사용하진 않음
+# \' 단일 인용부호
+# \" 이중 인용부호
+
+print("py"+"thon")  #python
+print("py" "thon")  #python
+print("py"*3)   #pypypy
+
+a = "python"
+print(a[0])     #p
+print(a[0:3])   #pyt
+print(a[:3])    #pyt
+print(a[-2:])   #on
+print(a[:])     #python
+
+strA = "python is powerful"
+print(strA[-1])
+print(strA[-3:])
+print(strA[-8:])
+
+
+# list(), int(), str() ****
+# Type Casting (형식 변환)
+
