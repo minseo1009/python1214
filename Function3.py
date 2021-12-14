05a646adae096db26c1201e01dcaf41e567ea8e1
minseo1009 <minseo@kribb.re.kr>
2021-12-14 11:49:33 +0900

    함수 연습4

--- Function3.py
+++ Function3.py
@@ -7,3 +7,13 @@ print(result)
 years = list(range(2000,2022))
 print(years)
 
+# -1 감소
+numbers = list(range(10,0,-1))
+print(numbers)
+
+# 리스트 컴프리헨션(한줄로 반복, 필터링, 가공하기)
+lst = list(range(1,11))
+result = [i**2 for i in lst if i > 5]
+print(result)
+
+# 필터링하는 함수
