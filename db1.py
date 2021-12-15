53645cb1b2615e99f50e5fd08d121313a3458463
minseo1009 <minseo@kribb.re.kr>
2021-12-15 16:47:46 +0900

    데이터베이스 연습

--- db1.py
+++ db1.py
@@ -2,20 +2,22 @@
 # 로컬 데이터베이스 엔진
 import sqlite3
 
-# 연결 객체 생성 : 임시로 메모리 저장
+# 연결 객체 생성 : 임시로 메모리 저장(Connection)
 con = sqlite3.connect(":memory:")
 
-# 커서에서 실제 SQL구문을 실행
+# 커서에서 실제 SQL구문을 실행(Cursor 클래스)
 cur = con.cursor()
 
-# 테이블 구조
-cur.execute("create table PhoneBook (Name text, PhoneNum text);")
+# 테이블 구조(테이블 스키마) : SQL은 대소문자 구분 안함
+# ANSI SQL 92, 99 표준안
+cur.execute("CREATE TABLE PhoneBook (Name text, PhoneNum text);")
 
 # 1건 입력
-cur.execute("insert into PhoneBook values ('derick', '010-222');")
+cur.execute("INSERT INTO PhoneBook VALUES ('derick', '010-222');")
 
 # 검색
-cur.execute("select * from PhoneBook;")
+cur.execute("SELECT * FROM PhoneBook;")
 for row in cur:
+    #print(row[0] +" , "+ row[1])
     print(row)
 
