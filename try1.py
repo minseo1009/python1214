27648b1eca439c00425e63e7941eaeecbb3aeca6
minseo1009 <minseo@kribb.re.kr>
2021-12-15 10:43:49 +0900

    에러처리

--- try1.py
+++ try1.py
@@ -4,20 +4,20 @@
 def divide(a,b):
     return a/b
 
-# 에러 처리
+# 에러 처리(화면단에서 처리)
 try:
     # 함수 호출
-    result = divide(5,0)
+    result = divide(5,"aaa")
     print("결과:{0}".format(result))
 except ZeroDivisionError:
     print("0으로 나누면 안됩니다.")
 except TypeError:
-    print("0으로 나누면 안됩니다.")
+    print("숫자여야 합니다.")
 else:
     print("결과:{0}".format(result))
 finally:
-    print("한번 더 체크(무조건 실행")
+    print("한번 더 체크(무조건 실행)")
 
-    
+
 print("전체 코드 실행 종료")
 
